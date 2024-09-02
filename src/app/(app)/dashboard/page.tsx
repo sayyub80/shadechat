e7bd647a813d0474018cc1a02afb5f2187fb7f18
sayyub80 <mdsayyub80@gmail.com>
2024-09-02 17:05:18 +0530

    sign-in page added

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+function page() {
+  return (
+    <div>
+       Dashboard
+    </div>
+  )
+}
+
+export default page
--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -9,7 +9,6 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-// import { useToast } from '@/components/ui/use-toast';
 import { useToast } from '@/hooks/use-toast';
 import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
